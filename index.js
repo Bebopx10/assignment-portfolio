1e7a1377b4246d7bccd8ee74d298c11ef53664e0
Bebopx10 <Jthobbs18@gmail.com>
2026-09-20 12:59:11 -0500

    Update Files

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-console.log("Starting Hello, World.")
+async function start() {
+	const response = await fetch("https://dog.ceo/api/breeds/list/all")
+	const data = await response.json()
+	createBreedList(data.message)
+}
 
-function makeGreen() {
-	console.log("Calling makeGreen function")
-	document.getElementById("msg").classList.add("green");
+start()
+
+function createBreedList() {
+	document.getElementById("breed").innerHTML = `
+	<select>
+		<option>Choose a dog breed</option>
+		${Object.keys(breedlist).map(function (breed) {
+			return `<option>${breed}</option>`
+		}).join('')}
+	</select>
+	`
 }
