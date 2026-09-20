f9ae3b0e01142b7ea99da078665e2291ddcaf12b
Bebopx10 <Jthobbs18@gmail.com>
2026-09-20 15:23:30 -0500

    Update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let timer
+let deleteFirstPhotoDelay
+
+
 async function start() {
 	const response = await fetch("https://dog.ceo/api/breeds/list/all")
 	const data = await response.json()
@@ -28,16 +32,28 @@ async function loadByBreed(breed) {
 
 function createSlideshow(images) {
 	let currentPosition = 0
-	document.getElementById("slideshow").innerHTML = `
+	clearInterval(timer)
+	clearTimeout(deleteFirstPhotoDelay)
+
+	if(images.length > 1){
+		document.getElementById("slideshow").innerHTML = `
 	<div class="slide" style="background-image: url('${images[0]}')"></div>
 	<div class="slide" style="background-image: url('${images[1]}')"></div>
 	`
 	currentPosition += 2
-	setInterval(nextSlide, 3000)
+	if (images.length == 2) currentPosition = 0
+	timer = setInterval(nextSlide, 3000)
+	}else{
+		document.getElementById("slideshow").innerHTML = `
+	<div class="slide" style="background-image: url('${images[0]}')"></div>
+	<div class="slide"></div>
+	`
+
+	}
 
 	function nextSlide() {
 		document.getElementById("Slideshow").insertAdjacentHTML("beforeend",`<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-		setTimeout(function () {
+		deleteFirstPhotoDelay = setTimeout(function () {
 			document.querySelector(".slide").remove()
 		}, 1000)
 			
